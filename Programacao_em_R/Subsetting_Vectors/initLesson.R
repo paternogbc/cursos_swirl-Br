50ec9cd7520de45d143336f39052bd7f2d287aa4
Gustavo B Paterno <paternogbc@gmail.com>
2015-09-02 12:09:09 -0300

    deleted

--- Programacao_em_R/Subsetting_Vectors/initLesson.R
+++
@@ -1,5 +0,0 @@
-# Put initialization code in this file. The variables you create
-# here will show up in the user's workspace when he or she begins
-# the lesson.
-
-x <- sample(c(rnorm(20), rep(NA, 20)))
